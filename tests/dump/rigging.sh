aad4a706eb6acea40208f03744bef57522e58c3e
Wendell Baker <wbaker@verizonmedia.com>
2019-04-24 12:51:56 -0700

    Version 0.0.0-0.0.devel, complete builds

--- tests/dump/rigging.sh
+++ tests/dump/rigging.sh
@@ -1,5 +1,5 @@
 # source ${0%/*}/rigging.sh || exit 70
 # ... just set the path (that is all, for now)
 #
-export PATH="${0%/*}/../../sbin:$PATH"
-exe=rockaway-service
+export PATH="${0%/*}/../../bin:$PATH"
+exe=dump-iab
